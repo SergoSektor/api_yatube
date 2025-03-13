2980495d40cec19baec5e55896e3904d12c83564
SergoSektor <serg.sektor.droid@gmail.com>
2025-03-14 03:18:09 +0700

    pep8

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -21,10 +21,12 @@ class GroupSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(slug_field='username', read_only=True)
-    post = serializers.PrimaryKeyRelatedField(read_only=True)
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
 
     class Meta:
         model = Comment
         fields = '__all__'
-        read_only_fields = ('author', 'post')
+
